a916c0f953be1b6a2a156d6be946d3a371ce227d
Chihab Eddine Derghal <chihabderghal1@gmail.com>
2025-01-31 01:11:10 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,29 @@
 import express, {Request, Response} from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { error } from "console";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { logger } from "./middlewares/logger";
+import { errorLogger } from "./middlewares/errorLogger";
+import authRouter from "./routes/authRoutes";
+import { connectDB } from "./config/dbConnection";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
+
+connectDB();
+
+app.use(express.json());
+app.use(logger);
+app.use(errorLogger);
+app.use(cors({
+    credentials: true,
+}));
+app.use("/auth", authRouter);
+app.use(cookieParser());
 
 app.get("/",  (req: Request, res, Response) => {
     res.status(200).json({ message: "Hello, World!"});
@@ -9,4 +31,16 @@ app.get("/",  (req: Request, res, Response) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+mongoose.connection.on("open", () => {
+    console.log("[INFO] Connected to MonogoDB");
+});
+
+mongoose.connection.on("error", () => {
+    console.log(error);
+});
+
+mongoose.connection.on("disconnected", () => {
+    console.log("[INFO]: Disconnected to MonogoDB");
 });

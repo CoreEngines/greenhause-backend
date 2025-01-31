5018ae73508e6d319a84039aedd6821c1a5b9fd2
Chihab Eddine Derghal <chihabderghal1@gmail.com>
2025-01-31 01:09:57 +0100

    Add User model

---
+++ src/models/users.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: false,
+        default: false,
+        select: false,
+    },
+});
+
+export default mongoose.model("User", userSchema);

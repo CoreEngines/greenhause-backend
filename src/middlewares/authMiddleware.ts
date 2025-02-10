a4a0201659b5233b0527aae8fc02720e2d4fef30
Chihab Derghal <chihabderghal1@gmail.com>
2025-02-10 19:24:21 +0100

    Add middleware for admin role verification

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { TokenPayLoad } from "../utils/jwt";
+import User from "../models/users";
 
 export function isAuthenticated(req: Request, res: Response, next: NextFunction): void {
     const accessToken = req.cookies.accessToken;
@@ -16,6 +17,33 @@ export function isAuthenticated(req: Request, res: Response, next: NextFunction)
         }
 
         next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Unauthorized" });
+    }
+}
+
+export async function isAdmin(req: Request, res: Response, next: NextFunction) {
+    const accessToken = req.cookies.accessToken;
+
+    if (!accessToken) {
+        res.status(401).json({ error: "Unauthorized" });
+    }
+
+    try {
+        const decode = jwt.verify(accessToken, process.env.JWT_AT_SECRET!) as TokenPayLoad;
+        console.log(decode);
+        if (!decode) {
+            res.status(401).json({ error: "Unauthorized" });
+        }
+
+        const user = await User.findById(decode.userId);
+        if (user && user.isAdmin) {
+            next();
+            return;
+        }
+
+        res.status(401).json({ error: "Unauthorized" });
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: "Unauthorized" });

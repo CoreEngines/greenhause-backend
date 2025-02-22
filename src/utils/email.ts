d14af2ba1c42633d288f9eb4b1d60fc17e97730b
Chihab Derghal <chihabderghal1@gmail.com>
2025-02-22 17:39:55 +0100

    Update email.ts

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend"
 import path from "path";
 import fs from "fs";
 
-let emailTemplateCache: string | null = null;
+const emailTemplateCache: Record<string, string> = {}; 
 
 export async function sendEmail(userEmail: string, subject: string, emailBody: string) {
     const resend = new Resend(process.env.RESEND_API_KEY!);
@@ -21,21 +21,27 @@ export async function sendEmail(userEmail: string, subject: string, emailBody: s
     };
 };
 
-export  function getEmailTemplate(link: string, token: string, template: string): string {
+export function getEmailTemplate(template: string, link?: string | null, token?: string | null): string {
     const filePath = path.join(__dirname, `../templates/${template}.html`);
 
     try {
-        if (!emailTemplateCache) {
-            emailTemplateCache =  fs.readFileSync(filePath, "utf8");
+        if (!emailTemplateCache[template]) {
+            emailTemplateCache[template] = fs.readFileSync(filePath, "utf8");
         }
 
-        const emailBody = emailTemplateCache
-            .replace(/{{TOKEN}}/g, token)
-            .replace(/{{LINK}}/g, link);
+        let emailBody = emailTemplateCache[template];
+
+        if (token) {
+            emailBody = emailBody.replace(/{{TOKEN}}/g, token);
+        }
+        
+        if (link) {
+            emailBody = emailBody.replace(/{{LINK}}/g, link);
+        }
 
         return emailBody;
     } catch (error) {
-        console.error("Error Reading email template", error);
+        console.error("Error reading email template:", error);
         throw new Error("Failed to load email template");
-    };
-};
+    }
+}

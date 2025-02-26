c5e7954ac52b9152d6437d0bd75220a39413d169
Chihab Derghal <chihabderghal1@gmail.com>
2025-02-26 16:08:40 +0100

    Add role to user schema

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,61 +1,64 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        default: null,
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
-    isDeleted: {
-        type: Boolean,
-        default: false,
-    },
-    providers: [{
-        providerId: {
+const userSchema = new mongoose.Schema(
+    {
+        name: {
             type: String,
             required: true,
-            default: null
         },
-        providerName: {
+        email: {
             type: String,
             required: true,
-            enum: ["local", "google","github"],
-            default: "local"
+            unique: true,
         },
-    }],
-    createdAt: {
-        type: Date,
-        default: Date.now(),
+        password: {
+            type: String,
+            default: null,
+        },
+        role: {
+            type: String,
+            default: null,
+            enum: ["manager", "farmer", "technician"],
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+        providers: [
+            {
+                providerId: {
+                    type: String,
+                    required: true,
+                    default: null,
+                },
+                providerName: {
+                    type: String,
+                    required: true,
+                    enum: ["local", "google", "github"],
+                    default: "local",
+                },
+            },
+        ],
+        deletedAt: {
+            type: Date,
+            default: null,
+        },
     },
-    updatedAt: {
-        type: Date,
-        default: Date.now(),
-    },
-    deletedAt: {
-        type: Date,
-        default: null,
-    },
-});
+    { timestamps: true }
+);
 
-const softDeletionPeriod = 60 * 5;// 5 minutes (for testing)
+const softDeletionPeriod = 60 * 5; // 5 minutes (for testing)
 
-userSchema.index({deletedAt: 1}, { expireAfterSeconds: softDeletionPeriod});
+userSchema.index({ deletedAt: 1 }, { expireAfterSeconds: softDeletionPeriod });
 
 userSchema.index({ "providers.providerId": 1 }, { unique: true, sparse: true });
 

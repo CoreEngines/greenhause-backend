fd7f1dd9ef58c6e5a45c4a73ec4b92ef55f2d708
Chihab Derghal <chihabderghal1@gmail.com>
2025-03-17 07:45:47 +0100

    Add Swagger docs to managerRoutes.ts

--- src/routes/managerRoutes.ts
+++ src/routes/managerRoutes.ts
@@ -3,8 +3,55 @@ import {AddFarmer, AddTechnician, getAllWorkers} from "../controllers/managerCon
 
 const managerRoutes = Router();
 
+/**
+ * @swagger
+ * /manager/add-farmer:
+ *   post:
+ *     summary: Add a farmer
+ *     tags: [Manager]
+ *     description: Adds a farmer to the system
+ *     responses:
+ *       200:
+ *         description: Farmer added successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 managerRoutes.post("/add-farmer", AddFarmer);
+
+/**
+ * @swagger
+ * /manager/add-technician:
+ *   post:
+ *     summary: Add a technician
+ *     tags: [Manager]
+ *     description: Adds a technician to the system
+ *     responses:
+ *       200:
+ *         description: Technician added successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ *
+ *
+ */
 managerRoutes.post("/add-technician", AddTechnician);
+
+/**
+ * @swagger
+ * /manager/workers:
+ *   get:
+ *     summary: Get all workers
+ *     tags: [Manager]
+ *     description: Retrieves a list of all workers
+ *     responses:
+ *       200:
+ *         description: List of workers retrieved successfully
+ *       500:
+ *         description: Internal server error
+ */
 managerRoutes.get("/workers", getAllWorkers);
 
 export default managerRoutes;
